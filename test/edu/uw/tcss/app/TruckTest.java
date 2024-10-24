eb56bcc6fa7d69cddb0bf647b16f3f1c3f372791
raidenh <raidenh@uw.edu>
2024-10-24 15:56:08 -0700

    updating TruckTest and Vehicle interface to specifications

--- test/edu/uw/tcss/app/TruckTest.java
+++ test/edu/uw/tcss/app/TruckTest.java
@@ -15,7 +15,10 @@ import edu.uw.tcss.model.Light;
 import edu.uw.tcss.model.Taxi;
 import edu.uw.tcss.model.Terrain;
 import edu.uw.tcss.model.Truck;
+import edu.uw.tcss.model.Vehicle;
 import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -28,39 +31,43 @@ public class TruckTest {
     /**
      * test map for testing chooseDirection().
      */
-    private static final HashMap<Direction, Terrain> TEST_MAP = new HashMap<>();
+    private static final Map<Direction, Terrain> TEST_MAP = new HashMap<>();
     /**
      * test Truck object for testing Truck methods.
      */
-    private static final Truck TEST_TRUCK = new Truck(0, 0, Direction.NORTH);
+    private Vehicle myTestTruck;
+    @BeforeEach
+    void setup() {
+        myTestTruck = new Truck(0, 0, Direction.NORTH);
+    }
 
     @Test
     void testGetImageFileName() {
-        assertEquals("truck.gif", TEST_TRUCK.getImageFileName(),
+        assertEquals("truck.gif", myTestTruck.getImageFileName(),
                 "Living truck image filename is incorrect.");
     }
     @Test
     void testCanPass() {
         assertAll("Testing canPass()",
-                () -> assertFalse(TEST_TRUCK.canPass(Terrain.GRASS, Light.RED),
+                () -> assertFalse(myTestTruck.canPass(Terrain.GRASS, Light.RED),
                         "Truck should not be able to pass grass."),
-                () -> assertTrue(TEST_TRUCK.canPass(Terrain.STREET, Light.GREEN),
+                () -> assertTrue(myTestTruck.canPass(Terrain.STREET, Light.GREEN),
                         "Truck should be able to pass streets."),
-                () -> assertFalse(TEST_TRUCK.canPass(Terrain.WALL, Light.GREEN),
+                () -> assertFalse(myTestTruck.canPass(Terrain.WALL, Light.GREEN),
                         "Truck should not be able to pass walls."),
-                () -> assertFalse(TEST_TRUCK.canPass(Terrain.TRAIL, Light.GREEN),
+                () -> assertFalse(myTestTruck.canPass(Terrain.TRAIL, Light.GREEN),
                         "Truck should not be able to pass trails."),
-                () -> assertTrue(TEST_TRUCK.canPass(Terrain.LIGHT, Light.GREEN),
+                () -> assertTrue(myTestTruck.canPass(Terrain.LIGHT, Light.GREEN),
                         "Truck should be able to pass green lights."),
-                () -> assertTrue(TEST_TRUCK.canPass(Terrain.LIGHT, Light.YELLOW),
+                () -> assertTrue(myTestTruck.canPass(Terrain.LIGHT, Light.YELLOW),
                         "Truck should be able to pass yellow lights."),
-                () -> assertTrue(TEST_TRUCK.canPass(Terrain.LIGHT, Light.RED),
+                () -> assertTrue(myTestTruck.canPass(Terrain.LIGHT, Light.RED),
                         "Truck should be able to pass red lights."),
-                () -> assertTrue(TEST_TRUCK.canPass(Terrain.CROSSWALK, Light.GREEN),
+                () -> assertTrue(myTestTruck.canPass(Terrain.CROSSWALK, Light.GREEN),
                         "Truck should be able to pass green-light crosswalks."),
-                () -> assertTrue(TEST_TRUCK.canPass(Terrain.CROSSWALK, Light.YELLOW),
+                () -> assertTrue(myTestTruck.canPass(Terrain.CROSSWALK, Light.YELLOW),
                         "Truck should be able to pass yellow-light crosswalks."),
-                () -> assertFalse(TEST_TRUCK.canPass(Terrain.CROSSWALK, Light.RED),
+                () -> assertFalse(myTestTruck.canPass(Terrain.CROSSWALK, Light.RED),
                         "Truck should not be able to pass red-light crosswalks.")
         );
     }
@@ -69,28 +76,28 @@ public class TruckTest {
     void testChooseDirection() {
         TEST_MAP.put(Direction.NORTH, Terrain.GRASS);
         assertAll("Testing truck chooseDirection()",
-                () -> assertNotEquals(Direction.SOUTH, TEST_TRUCK.chooseDirection(TEST_MAP),
+                () -> assertNotEquals(Direction.SOUTH, myTestTruck.chooseDirection(TEST_MAP),
                         "Truck should prefer any direction but backwards.")
         );
     }
 
     @Test
     void testCollide() {
-        TEST_TRUCK.collide(new Car(0, 0, Direction.NORTH));
-        assertTrue(TEST_TRUCK.isAlive(), "Truck should survive collision with Car");
-        TEST_TRUCK.collide(new Taxi(0, 0, Direction.NORTH));
-        assertTrue(TEST_TRUCK.isAlive(), "Truck should survive collision with Taxi");
-        TEST_TRUCK.collide(new Atv(0, 0, Direction.NORTH));
-        assertTrue(TEST_TRUCK.isAlive(), "Truck should survive collision with Atv");
-        TEST_TRUCK.collide(new Bicycle(0, 0, Direction.NORTH));
-        assertTrue(TEST_TRUCK.isAlive(), "Truck should survive collision with Bicycle");
-        TEST_TRUCK.collide(new Human(0, 0, Direction.NORTH));
-        assertTrue(TEST_TRUCK.isAlive(), "Truck should survive collision with Human");
+        myTestTruck.collide(new Car(0, 0, Direction.NORTH));
+        assertTrue(myTestTruck.isAlive(), "Truck should survive collision with Car");
+        myTestTruck.collide(new Taxi(0, 0, Direction.NORTH));
+        assertTrue(myTestTruck.isAlive(), "Truck should survive collision with Taxi");
+        myTestTruck.collide(new Atv(0, 0, Direction.NORTH));
+        assertTrue(myTestTruck.isAlive(), "Truck should survive collision with Atv");
+        myTestTruck.collide(new Bicycle(0, 0, Direction.NORTH));
+        assertTrue(myTestTruck.isAlive(), "Truck should survive collision with Bicycle");
+        myTestTruck.collide(new Human(0, 0, Direction.NORTH));
+        assertTrue(myTestTruck.isAlive(), "Truck should survive collision with Human");
     }
 
     @Test
     void testIsAlive() {
-        assertTrue(TEST_TRUCK.isAlive(),
+        assertTrue(myTestTruck.isAlive(),
                 "Truck should always be alive");
     }
 }
