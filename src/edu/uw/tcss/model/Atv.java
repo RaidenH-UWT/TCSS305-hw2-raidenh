f321c248ab6975cb76efcf67728c0254554e2e6e
raidenh <raidenh@uw.edu>
2024-10-25 14:14:47 -0700

    Implemented Atv vehicle

--- src/edu/uw/tcss/model/Atv.java
+++ src/edu/uw/tcss/model/Atv.java
@@ -8,17 +8,28 @@ import java.util.Map;
  * @version 23-10-2024
  */
 public class Atv extends AbstractVehicle {
+    /**
+     * Constructor for the Atv class.
+     *
+     * @param theX x coordinate of the created atv
+     * @param theY x coordinate of the created atv
+     * @param theDir direction the created atv is facing
+     */
     public Atv(final int theX, final int theY, final Direction theDir) {
         super(theX, theY, theDir, 25);
     }
 
     @Override
-    public boolean canPass(Terrain theTerrain, Light theLight) {
-        return false;
+    public boolean canPass(final Terrain theTerrain, final Light theLight) {
+        return theTerrain != Terrain.WALL;
     }
 
     @Override
-    public Direction chooseDirection(Map<Direction, Terrain> theNeighbors) {
-        return null;
+    public Direction chooseDirection(final Map<Direction, Terrain> theNeighbors) {
+        Direction choice = getDirection().reverse();
+        while (choice == getDirection().reverse()) {
+            choice = Direction.random();
+        }
+        return choice;
     }
 }
