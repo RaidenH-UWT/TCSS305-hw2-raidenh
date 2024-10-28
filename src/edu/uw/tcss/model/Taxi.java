558725621f28ff4344fadae974b22db778dd70bd
raidenh <raidenh@uw.edu>
2024-10-28 10:32:05 -0700

    implemented Taxi class

--- src/edu/uw/tcss/model/Taxi.java
+++ src/edu/uw/tcss/model/Taxi.java
@@ -15,6 +15,10 @@ public class Taxi extends AbstractVehicle {
     private static final EnumSet<Terrain> ALLOWED_TERRAIN = EnumSet.of(
             Terrain.STREET, Terrain.LIGHT, Terrain.CROSSWALK);
     /**
+     * how long the taxi will wait at a red crosswalk
+     */
+    private int myPatience = 3;
+    /**
      * Constructor for the Taxi class.
      *
      * @param theX x coordinate of the created taxi
@@ -27,11 +31,30 @@ public class Taxi extends AbstractVehicle {
 
     @Override
     public boolean canPass(final Terrain theTerrain, final Light theLight) {
-        return false;
+        boolean result = ALLOWED_TERRAIN.contains(theTerrain);
+        if (theTerrain == Terrain.LIGHT && theLight == Light.RED) {
+            result = false;
+        } else if (theTerrain == Terrain.CROSSWALK && theLight == Light.RED) {
+            if (myPatience == 0) {
+                myPatience = 3;
+            } else {
+                result = false;
+                myPatience--;
+            }
+        }
+        return result;
     }
 
     @Override
     public Direction chooseDirection(final Map<Direction, Terrain> theNeighbors) {
-        return null;
+        Direction choice = getDirection().reverse();
+        if (ALLOWED_TERRAIN.contains(theNeighbors.get(getDirection()))) {
+            choice = getDirection();
+        } else if (ALLOWED_TERRAIN.contains(theNeighbors.get(getDirection().left()))) {
+            choice = getDirection().left();
+        } else if (ALLOWED_TERRAIN.contains(theNeighbors.get(getDirection().right()))) {
+            choice = getDirection().right();
+        }
+        return choice;
     }
 }
